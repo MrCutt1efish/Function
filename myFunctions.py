9b919d8ab7723ae792edd0ec28e2c405144f6258
MrCutt1efish <benjaminsecrest@mail.weber.edu>
2019-11-13 07:49:13 -0700

    trying

---
+++ myFunctions.py
@@ -0,0 +1,32 @@
+# DRY Do not Repeat
+
+# names = ['Scott', 'Billy', 'Susy', 'Jane']
+
+# print('First Name: ' + names[0])
+# print('First Name: ' + names[1])
+# print('First Name: ' + names[2])
+# print('First Name: ' + names[3])
+
+# for name in names:
+#     print('First Name is ' + name)
+
+# Create a converter for units of length
+# Convert inches to mm 25.4 mm in 1 in
+# Convert mm to inches 1/25.4
+
+# inchesToConvert = initial inches
+# convertedMM = converted mm
+
+def getUserInput():
+    userInput = input('Please enter the length? ')
+    return userInput
+
+inchesToConvert = getUserInput()
+convertedMM = float(inchesToConvert) * 25.4
+print(" The conversion = " + str(convertedMM))
+# mmToConvert = initial mm
+# convertedIN = converted in
+
+mmToConvert = getUserInput()
+convertedIN = float(mmToConvert) / 25.4
+print(" The conversion = " + str(convertedIN))
